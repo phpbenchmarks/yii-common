3c74a03e1db58707f35da749a795bd455ecf0d49
jcheron <myaddressmail@gmail.com>
2019-01-17 15:12:38 +0100

    Add controller test

---
+++ config/controller-map.php
@@ -0,0 +1,5 @@
+<?php
+Yii::setAlias('PhpBenchmarksYiiRest', __DIR__ . '/../');
+return [
+		'restapi' => ['class'=>'PhpBenchmarksYiiRest\controllers\RestapiController'],
+];
---
+++ controllers/RestapiController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace PhpBenchmarksYiiRest\controllers;
+
+use yii\web\Controller;
+
+class RestapiController extends Controller
+{
+ 
+    /**
+     *
+     * @return string
+     */
+    public function actionIndex()
+    {
+        return "rest";
+    }
+}
