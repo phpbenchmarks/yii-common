052ca0d696223b9085b4bc383befcb205c61af0e
Steevan <steevan.barboyon@gmail.com>
2019-04-28 19:02:44 +0200

    Fix Hello World ! return

--- controllers/HelloworldController.php
+++ controllers/HelloworldController.php
@@ -6,13 +6,11 @@ use yii\web\Controller;
 
 class HelloworldController extends Controller
 {
- 
     /**
-     *
      * @return string
      */
     public function actionIndex()
     {
-        return "Hello world ! ";
+        return "Hello World !";
     }
 }
